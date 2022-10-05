1097143455968c2619e1d366e7c9c66fba00436d
Sewentres <Kszmi12@gmail.com>
2022-10-05 15:37:11 +0200

    add doc string to fix all pylint problems

--- src/srecord.py
+++ src/srecord.py
@@ -1,13 +1,40 @@
 """Main python starting script for Hex modification
 """
+import os
 
 
 class HexFile:
     """Class for funcionality of formatting srecord files, will be used also for another formats"""
 
-    def __init__(self, File_Path):
-        print(rf"Hex file: {File_Path}")
-        self.File = File_Path
+    def __init__(self, file_path):
+        print(rf"Hex file: {file_path}")
+        self._file = file_path
 
-    def format_file(self):
-        print("test")
+    @property
+    def file(self):
+        """Path to hex file
+
+        Returns:
+            string: Path to file
+        """
+        return self._file
+
+    @property
+    def file_name(self):
+        """Name of hex file
+
+        Returns:
+            string: Striped name of file
+        """
+        return os.path.basename(self._file)
+
+    def format_file(self, file_path, format_hex):
+        """Function to convert a hex file to different format like Intel Hex,
+        Motorola Srecord or Binary.
+
+        Args:
+            file_path (str): Path to input file
+            format_hex (str): Format of output file
+        """
+        print(rf"Input file = [{file_path}]")
+        print(rf"Format_hex = [{format_hex}]")
