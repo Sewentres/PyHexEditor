57a557ad5140e87edf939c09592a25f92f3f15a0
Sewentres <Kszmi12@gmail.com>
2022-11-13 11:35:09 +0100

    Add test runner module, split input parser to modu

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
 """Main python starting script for Hex modification
 """
-import argparse
-from src import HexFile
 import os
-
-
-def cmdline_args():
-    """Parser for input arguments.
-
-    Returns:
-        object: all parsered arguments.
-    """
-    # Make parser object
-    parser = argparse.ArgumentParser(
-        description="", formatter_class=argparse.RawDescriptionHelpFormatter
-    )
-
-    parser.add_argument("--PATH", help="Path to hex file", required=True)
-
-    return parser.parse_args()
-
+from unittest import TestLoader, TestResult
+from pathlib import Path
+from src import HexFile, ArgumentsInputs
+from tests import TestRunner
 
 if __name__ == "__main__":
 
-    args = cmdline_args()
-    Hex = HexFile(args.PATH)
+    args = ArgumentsInputs()
+    if args.unit_tests:
+        unit_tests = TestRunner()
+
+    Hex = HexFile(args.path)
     # for data in Hex.s19_records:
     #     print(data)
     Hex.find_record_by_address("0x045180")
---
+++ src/input_parser.py
@@ -0,0 +1,41 @@
+import argparse
+
+
+class ArgumentsInputs:
+    """Class for arguments from user"""
+
+    def __init__(self):
+        self.cmdline_args()
+
+    def cmdline_args(self):
+        """Parser for input arguments.
+
+        Returns:
+            object: all parsered arguments.
+        """
+        # Make parser object
+        parser = argparse.ArgumentParser(
+            description="", formatter_class=argparse.RawDescriptionHelpFormatter
+        )
+
+        parser.add_argument("--PATH", help="Path to hex file", required=False)
+        parser.add_argument(
+            "--UNITTESTS",
+            help="Perform unit tests",
+            required=False,
+            action=argparse.BooleanOptionalAction,
+        )
+        arg = parser.parse_args()
+        self.path = arg.PATH
+        self.unit_tests = arg.UNITTESTS
+        if not self.unit_tests:
+            if not self.path:
+                print("************************************************")
+                print("*** Path to binary file was not given")
+                print(
+                    "*** Provide path to binary file using parameter --PATH <path_to_file>"
+                )
+                print("*** Or use --UNITTESTS parameter to run available unit tests")
+                print("************************************************")
+                exit(1)
+        return True
